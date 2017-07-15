845d029fff42d6ca8e6631b5d3ceb43709ee2472
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-07-15 13:11:40 +0900

    [add] UserInputManagerの作成
    各種UserInputクラスの管理

---
+++ Assets/Scripts/UserInputManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using UserInputs;
+
+public enum UserInputCode
+{
+  AddSong,
+  FastForward,
+  Pause,
+  Playback,
+  Restart,
+  Rewind,
+  Stop
+}
+
+public class UserInputManager : MonoBehaviour
+{
+  static UserInput[] userInput = new UserInput[Enum.GetNames(typeof(UserInputCode)).Length];
+
+  void Start()
+  {
+    userInput[(int)UserInputCode.AddSong] = new AddSong();
+    userInput[(int)UserInputCode.FastForward] = new FastForward();
+    userInput[(int)UserInputCode.Pause] = new Pause();
+    userInput[(int)UserInputCode.Playback] = new Playback();
+    userInput[(int)UserInputCode.Restart] = new Restart();
+    userInput[(int)UserInputCode.Rewind] = new Rewind();
+    userInput[(int)UserInputCode.Stop] = new Stop();
+  }
+
+  void Update()
+  {
+    foreach (UserInputCode code in Enum.GetValues(typeof(UserInputCode)))
+    {
+      userInput[(int)code].Update();
+    }
+  }
+
+  public static bool GetInput(UserInputCode code)
+  {
+    return userInput[(int)code].GetUpdateFlag();
+  }
+}
