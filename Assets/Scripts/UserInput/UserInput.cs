e34b9849cf5e3a95d4d4c9922fb9044997e27461
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-07-15 12:28:24 +0900

    [add] UserInputの抽象クラスを作成

---
+++ Assets/Scripts/UserInput/UserInput.cs
@@ -0,0 +1,32 @@
+﻿namespace UserInputs
+{
+  public abstract class UserInput
+  {
+    bool updateFlag = false; // 受信フラグ
+
+    public bool GetUpdateFlag()
+    {
+      return updateFlag;
+    }
+
+    public void DataUpdated()
+    {
+      updateFlag = true;
+    }
+
+    public void Reset()
+    {
+      updateFlag = false;
+      ResetData();
+    }
+
+    public void Update()
+    {
+      Reset();
+      UpdateData();
+    }
+
+    protected abstract void ResetData(); // 中身のデータのリセット
+    protected abstract void UpdateData(); // データのアップデート
+  }
+}
