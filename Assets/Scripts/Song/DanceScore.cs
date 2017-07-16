dd6e59a48c501275f5f802649b49a81a7f665870
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-07-16 09:02:24 +0900

    [add] Songクラス仮実装

---
+++ Assets/Scripts/Song/DanceScore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SongUtility
+{
+  enum ScoreName
+  {
+    Smile,
+    Jump,
+    Extensiveness,
+    Intensity,
+    Rhythm
+  }
+
+  public class DanceScore
+  {
+    int[] score = new int[Enum.GetNames(typeof(ScoreName)).Length];
+  }
+}
---
+++ Assets/Scripts/Song/Song.cs
@@ -0,0 +1,18 @@
+﻿namespace SongUtility
+{
+  public class Song
+  {
+    int songId; // 曲ID
+    string title; // 曲タイトル
+    string artist; // アーティスト名
+    DanceScore danceScore; // 採点
+    Movie movie; // 動画
+
+    public Song(int id)
+    {
+      danceScore = new DanceScore();
+      movie = new Movie();
+      songId = id;
+    }
+  }
+}
