b81f09495ca56110409f7136fb868559f6bf8c38
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-07-20 23:03:37 +0900

    [add] PlaySceneManagerの作成

---
+++ Assets/Scripts/PlayScene/PlaySceneManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlaySceneManager : MonoBehaviour
+{
+  void Start ()
+  {
+    SceneManager.LoadScene("DanceMovie", LoadSceneMode.Additive);
+    SceneManager.LoadScene("SongInfomation", LoadSceneMode.Additive);
+  }
+}
