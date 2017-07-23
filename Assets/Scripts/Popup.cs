cb0385d95b30f666f50bdef1c7dd9629b46dd93c
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-07-23 23:51:47 +0900

    [add] 曲追加時にpopup prefabを表示する

--- Assets/Scripts/Popup.cs
+++ Assets/Scripts/Popup.cs
@@ -1,6 +1,45 @@
 ﻿﻿using UnityEngine;
+using UnityEngine.UI;
+using SongUtility;
 using UserInputs;
 
 public class Popup : MonoBehaviour
 {
+  public GameObject addSongPopup;
+  GameObject popup;
+  AddSong addSong;
+
+  void Start ()
+  {
+    addSong = (AddSong)UserInputManager.GetUserInput(UserInputCode.AddSong);
+    addSong.OnValueChanged += OnAddSongValueChanged;
+  }
+
+  // 曲追加時のポップアップ
+  void OnAddSongValueChanged(object sender, UserInputEventArgs e)
+  {
+    Clear();
+    popup = Instantiate(
+              addSongPopup,
+              transform.position,
+              Quaternion.identity
+            );
+    popup.transform.SetParent(transform);
+
+    Song song = addSong.GetData(0);
+
+    Text songTitle = popup.transform.Find("SongTitle").gameObject.GetComponent<Text>();
+    songTitle.text = song.title;
+    Text artistName = popup.transform.Find("ArtistName").gameObject.GetComponent<Text>();
+    artistName.text = song.artist;
+  }
+
+  // 出ているポップアップの削除
+  void Clear()
+  {
+    if (popup != null)
+    {
+      Destroy(popup);
+    }
+  }
 }
--- Assets/Scripts/Song/Song.cs
+++ Assets/Scripts/Song/Song.cs
@@ -3,10 +3,37 @@
   public class Song
   {
     int songId; // 曲ID
-    string title; // 曲タイトル
-    string artist; // アーティスト名
+    string _title; // 曲タイトル
+    string _artist; // アーティスト名
     DanceScore danceScore; // 採点
     Movie movie; // 動画
+
+    public string title
+    {
+      get
+      {
+        return _title;
+      }
+
+      private set
+      {
+        _title = value;
+      }
+    }
+
+
+    public string artist
+    {
+      get
+      {
+        return _artist;
+      }
+
+      private set
+      {
+        _artist = value;
+      }
+    }
 
     public Song(int id)
     {
