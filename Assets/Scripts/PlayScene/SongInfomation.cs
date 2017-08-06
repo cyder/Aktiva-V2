af3b08c00cb69ca3a143788933e66765cee0e583
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-08-06 16:30:48 +0900

    [add] 動画の最初にタイトルを表示

---
+++ Assets/Scripts/PlayScene/SongInfomation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SongUtility;
+
+public class SongInfomation : MonoBehaviour
+{
+  void Start ()
+  {
+    Song song = SongManager.getNowPlaySong();
+    var songTitle = this.transform.Find("SongTitle").gameObject.GetComponent<Text>();
+    songTitle.text = song.title;
+    var artistName = this.transform.Find("ArtistName").gameObject.GetComponent<Text>();
+    artistName.text = song.artist;
+    StartCoroutine(DestroyObjectDelay(3.0f, this.gameObject));
+  }
+
+  // 一定時間後に削除
+  IEnumerator DestroyObjectDelay(float waitTime, GameObject target)
+  {
+    yield return new WaitForSeconds(waitTime);
+    Destroy(target);
+  }
+}
